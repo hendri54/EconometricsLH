63d058c3d9ad267a3523fec2a4cd848bb260d22c
hendri54 <hendricksl@protonmail.com>
2019-09-27 13:39:44 -0400

    export has_regressor

--- src/EconometricsLH.jl
+++ src/EconometricsLH.jl
@@ -10,9 +10,9 @@ export RegressorInfo, RegressionTable
 export reduce_regr_infos
 
 # RegressionTable
-export add_regressor, change_regressor!, drop_regressor!, drop_regressors!
+export add_regressor, get_regressor, has_regressor
+export change_regressor!, drop_regressor!, drop_regressors!
 export get_coefficient, get_std_error, get_coeff_se, get_coeff_se_multiple, get_all_coeff_se
-export get_regressor, has_regressor
 export get_names, get_name_strings, assert_same_regressors, have_same_regressors
 export n_regressors, reduce_regr_tables, rename_regressor, set_missing_regressors!
 
